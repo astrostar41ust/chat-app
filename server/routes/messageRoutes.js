1cd593fd8ff7bc235266ec05e3e76be73c185017
astrostar41ust <astrostar41ust@gmail.com>
2025-08-31 01:21:32 +0700

    complete ui

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -11,6 +11,6 @@ const messageRouter = express.Router();
 
 messageRouter.get("/users", protectRoute, getUserForSidebar);
 messageRouter.get("/:id", protectRoute, getMessages);
-messageRouter.get("/mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
 messageRouter.post("/send/:id", protectRoute, sendMessage)
 export default messageRouter
